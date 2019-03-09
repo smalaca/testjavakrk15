5dd639d2109ec85cc1da1f36d035350ada8ef667
smalaca <ab13.krakow@gmail.com>
2019-03-09 12:15:29 +0100

    Add tests to verify if Course started before given date

--- src/test/java/com/smalaca/CourseTest.java
+++ src/test/java/com/smalaca/CourseTest.java
@@ -5,25 +5,39 @@ import org.junit.Test;
 import java.time.LocalDate;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class CourseTest {
+
+    private static final String ANY_COURSE_NAME = "Course Name";
+
     @Test
     public void shouldHaveTheSameName() {
-        String anyName = "Course Name";
-        Course course = new Course(anyName, LocalDate.now());
+        Course course = new Course(ANY_COURSE_NAME, LocalDate.now());
 
         String result = course.name();
 
-        assertEquals(anyName, result);
+        assertEquals(ANY_COURSE_NAME, result);
     }
 
     @Test
-    public void shouldHaveStartDateBefore() {
+    public void shouldHaveStartedBeforeGivenDate() {
+        Course course = new Course(ANY_COURSE_NAME, LocalDate.of(2019, 3, 1));
+        LocalDate presentDate = LocalDate.now();
 
+        boolean result = course.wasStartedBefore(presentDate);
+
+        assertTrue(result);
     }
 
     @Test
     public void shouldHaveStartDateNotBefore() {
+        Course course = new Course(ANY_COURSE_NAME, LocalDate.of(2019, 3, 1));
+        LocalDate presentDate = LocalDate.of(2018, 3, 1);
 
+        boolean result = course.wasStartedBefore(presentDate);
+
+        assertFalse(result);
     }
 }
